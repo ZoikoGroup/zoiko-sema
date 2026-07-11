8f00fc1ccd643b2dd0ba5748aa50ddf9eeefaaf1
unknown <adityaN@zoikogroup.com>
2026-07-11 15:48:15 +0530

    modified index.ts

--- components/Personal-to-team/index.ts
+++ components/Personal-to-team/index.ts
@@ -1,4 +1,4 @@
-export { default as PersonalToTeamPage } from "../../app/Personal-to-team/PersonalToTeamPage";
+
 export { GovernanceControlsSection } from './GovernanceControlsSection';
 export { IntegrationsEcosystemSection } from './IntegrationsEcosystemSection';
 export { TestimonialStatsSection } from './TestimonialStatsSection';
